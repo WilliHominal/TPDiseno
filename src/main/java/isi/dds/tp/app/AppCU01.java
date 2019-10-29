d1b5b5b5cef7791420a2ac43e1da986507b9619c
Milton Bernhardt <38256800+miltonbernhardt@users.noreply.github.com>
2019-10-28 21:44:19 -0300

    Add files via upload

---
+++ src/main/java/isi/dds/tp/app/AppCU01.java
@@ -0,0 +1,23 @@
+package isi.dds.tp.app;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import isi.dds.tp.controller.CU01Controller;
+import isi.dds.tp.gestor.GestorCliente;
+
+public class AppCU01 {
+	 public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame();
+					CU01Controller a = new CU01Controller(frame);
+					a.obtenidoCliente(GestorCliente.get().getCliente(5448044249l));
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
---
+++ src/main/java/isi/dds/tp/app/AppCU17.java
@@ -0,0 +1,20 @@
+package isi.dds.tp.app;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import isi.dds.tp.controller.CU17Controller;
+public class AppCU17 {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame();
+					new CU17Controller(frame);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
