fb8dd0907f6bc97351d0155ec6069e7bc3bd5008
Milton Bernhardt <38256800+miltonbernhardt@users.noreply.github.com>
2019-10-31 18:26:27 -0300

    Add files via upload

---
+++ src/main/java/isi/dds/tp/view/CU18View2.java
@@ -0,0 +1,15 @@
+package isi.dds.tp.view;
+
+import javax.swing.JPanel;
+import isi.dds.tp.gestor.GestorTema;
+
+public class CU18View2   extends JPanel{
+	private static final long serialVersionUID = -1777108347932069403L;
+
+@SuppressWarnings("unused")
+private GestorTema tema = GestorTema.get();
+	
+	public CU18View2() {
+		
+	}
+}
