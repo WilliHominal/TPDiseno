51d253b5409430fa14e7ec88e3a514ed7a8dbad7
padu818 <57108306+padu818@users.noreply.github.com>
2019-11-17 00:00:04 -0300

    quite un /*

--- src/main/java/isi/dds/tp/controller/CU12Controller1.java
+++ src/main/java/isi/dds/tp/controller/CU12Controller1.java
@@ -190,9 +190,9 @@ public class CU12Controller1 {
 		public void actionPerformed(ActionEvent e) {
 			try {
 				//TODO MODIFICAR CON CU18
-				CU18Controller a = new CU18Controller(ventana);	
+				//CU18Controller a = new CU18Controller(ventana);	
 				//a.setPagoPolizaController(instancia); // ahi aplicar el obtenidaPoliza(con la poliza que se selecciona)
-				*/
+				
 				
 				// el cliente 5400000004 tiene la poliza-> 3528000000401 con cuotas impagas, pero tiene las primeras pagas
 				// el cliente 5400000003 tiene la poliza 3528000000300 con todas las cuotas impagas
